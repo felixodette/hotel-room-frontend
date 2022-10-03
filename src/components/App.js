27574d2fe3a6624c66edc75aa70c996a7274e097
Huseyin Biyik <tahahuseyinb@gmail.com>
2022-10-03 12:12:40 +0300

    Update and move App.js file

---
+++ src/components/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+import React from 'react';
+import { Routes, Route, BrowserRouter } from 'react-router-dom';
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        {/* <Route path="/" element={<Rooms />} />
+        <Route path="/" element={<Room />} />
+        <Route path="/reservations" element={<MyReservations />} />
+        <Route path="/reservations-new" element={<ReservationForm />} />
+        <Route path="/room-new" element={<AddRoom />} />
+        <Route path="/room-list" element={<IndexRemoveRoom />} /> */}
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
