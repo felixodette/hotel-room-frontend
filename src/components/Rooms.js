7eb6c3a348ae63d332fe19f140ee581778e59aed
Ahmad Zamir Yousufi <ahmadzamiryousofi1397@gmail.com>
2022-10-04 18:14:23 +0430

    modify the Rooms component to add the redux store state data for rooms and fetch the rooms data while loading the page

--- src/components/Rooms.js
+++ src/components/Rooms.js
@@ -1,43 +1,32 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import { useSelector, useDispatch } from 'react-redux';
+import { getRooms } from '../redux/rooms';
 
 const Rooms = () => {
-  const rooms = [
-    {
-      id: 1,
-      name: 'Room 1',
-      description: 'Room 1 description',
-      image: 'https://picsum.photos/200/300',
-    },
-    {
-      id: 2,
-      name: 'Room 2',
-      description: 'Room 2 description',
-      image: 'https://picsum.photos/200/300',
-    },
-    {
-      id: 3,
-      name: 'Room 3',
-      description: 'Room 3 description',
-      image: 'https://picsum.photos/200/300',
-    },
-  ];
-  return (
-    <div>
-      <h1>Rooms</h1>
-      <h2>Plese select a room</h2>
-      <div className="rooms">
-        {rooms.map((room) => (
-          <div className="room" key={room.id}>
-            <img src={room.image} alt={room.name} />
-            <div className="room-info">
-              <h3>{room.name}</h3>
-              <p>{room.description}</p>
-            </div>
-          </div>
-        ))}
-      </div>
-    </div>
-  );
+	const dispatch = useDispatch();
+	const rooms = useSelector((state) => state.rooms.rooms);
+
+	useEffect(() => {
+		dispatch(getRooms());
+	}, [dispatch]);
+	return (
+		<div>
+			<h1>Rooms</h1>
+			<h2>Plese select a room</h2>
+			<div className="rooms">
+				{rooms.map((room) => (
+					<div className="room" key={room.id}>
+						<img src={room.image} alt={room.name} />
+						<div className="room-info">
+							<h3>{room.name}</h3>
+							<p>{room.description}</p>
+						</div>
+					</div>
+				))}
+			</div>
+		</div>
+	);
 };
 
 export default Rooms;
