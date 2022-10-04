1a31ebae711f15aaf390d207af50e28c5609d3a2
Ahmad Zamir Yousufi <ahmadzamiryousofi1397@gmail.com>
2022-10-04 16:27:18 +0430

    add the rooms component in the root path as a route

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,14 @@
 import '../App.css';
 import React from 'react';
 import { Routes, BrowserRouter } from 'react-router-dom';
-// import {  Route } from 'react-router-dom';
+import { Route } from 'react-router-dom';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        {/* <Route path="/" element={<Rooms />} />
+        {<Route path="/" element={<Rooms />} />
+        /* 
         <Route path="/" element={<Room />} />
         <Route path="/reservations" element={<MyReservations />} />
         <Route path="/reservations-new" element={<ReservationForm />} />
