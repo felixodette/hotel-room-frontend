002bd29a83ce3e5ffa43afc4201e11806a978c8a
Huseyin Biyik <tahahuseyinb@gmail.com>
2022-10-04 14:25:23 +0300

    Add AddRoom route

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,18 @@
 import '../App.css';
 import React from 'react';
-import { Routes, BrowserRouter } from 'react-router-dom';
-// import {  Route } from 'react-router-dom';
+import { Routes, BrowserRouter, Route } from 'react-router-dom';
+import AddRoom from './Rooms/AddRoom';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        {/* <Route path="/" element={<Rooms />} />
-        <Route path="/" element={<Room />} />
-        <Route path="/reservations" element={<MyReservations />} />
-        <Route path="/reservations-new" element={<ReservationForm />} />
+        {/* <Route path="/" element={<Rooms />} /> */}
+        {/* <Route path="/" element={<Room />} /> */}
+        {/* <Route path="/reservations" element={<MyReservations />} /> */}
+        {/* <Route path="/reservations-new" element={<ReservationForm />} /> */}
         <Route path="/room-new" element={<AddRoom />} />
-        <Route path="/room-list" element={<IndexRemoveRoom />} /> */}
+        {/* <Route path="/room-list" element={<IndexRemoveRoom />} /> */}
       </Routes>
     </BrowserRouter>
   );
