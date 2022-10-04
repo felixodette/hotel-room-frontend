f0e516d65d34029b9751d111e02a23a53cdde1dc
Precious Akams <preciousakams@yahoo.com>
2022-10-04 18:41:46 +0100

    user setup

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,7 @@ import React from 'react';
 import { Routes, BrowserRouter, Route } from 'react-router-dom';
 import AddRoom from './Rooms/AddRoom';
 import Rooms from './Rooms';
+import AddUser from './user';
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
         {/* <Route path="/reservations-new" element={<ReservationForm />} /> */}
         <Route path="/room-new" element={<AddRoom />} />
         {/* <Route path="/room-list" element={<IndexRemoveRoom />} /> */}
+        <Route path="/user" element={<AddUser />} />
       </Routes>
     </BrowserRouter>
   );
