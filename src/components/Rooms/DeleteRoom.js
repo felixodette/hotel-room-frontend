74737a163d75d399d6b5b4a78dcf58c80d3cb13d
Huseyin Biyik <tahahuseyinb@gmail.com>
2022-10-04 18:29:50 +0300

    Add listing room functionality

---
+++ src/components/Rooms/DeleteRoom.js
@@ -0,0 +1,33 @@
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { getRooms } from '../../redux/rooms';
+
+const DeleteRoom = () => {
+  const dispatch = useDispatch();
+  const rooms = useSelector((state) => state.rooms.rooms);
+
+  useEffect(() => {
+    dispatch(getRooms());
+  }, [dispatch]);
+  return (
+    <div>
+      <h1>Rooms</h1>
+      <h2>Plese select a room</h2>
+      <div className="rooms">
+        {rooms.map((room) => (
+          <a href={`/room/${room.id}`} key={room.id}>
+            <div className="room" key={room.id}>
+              <img src={room.image} alt={room.name} />
+              <div className="room-info">
+                <h3>{room.name}</h3>
+                <p>{room.description}</p>
+              </div>
+            </div>
+          </a>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default DeleteRoom;
