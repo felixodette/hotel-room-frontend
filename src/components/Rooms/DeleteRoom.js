5bd75fd190091e8b2c079f2e32495f2feeee9ce4
Huseyin Biyik <tahahuseyinb@gmail.com>
2022-10-05 14:41:25 +0300

    Add redux action and dispatch method for deleting room

--- src/components/Rooms/DeleteRoom.js
+++ src/components/Rooms/DeleteRoom.js
@@ -1,18 +1,19 @@
 import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { getRooms } from '../../redux/rooms';
+import { getRooms, deleteRoom } from '../../redux/rooms';
 
 const DeleteRoom = () => {
   const dispatch = useDispatch();
   const rooms = useSelector((state) => state.rooms.rooms);
 
-  const requestOptions = {
-    method: 'DELETE',
-    headers: { 'Content-type': 'application/json; charset=UTF-8' },
-  };
+  const deleteRoomHandler = (id) => {
+    const requestOptions = {
+      method: 'DELETE',
+      headers: { 'Content-type': 'application/json; charset=UTF-8' },
+    };
 
-  const deleteRoom = (id) => {
-    fetch(`http://localhost:3000/api/v1/rooms/${id}`, requestOptions);
+    fetch(`http://localhost:3000/api/v1/rooms/${id}`, requestOptions)
+      .then(dispatch(deleteRoom(id)));
   };
 
   useEffect(() => {
@@ -27,7 +28,7 @@ const DeleteRoom = () => {
             <h3>{room.name}</h3>
             <button
               type="button"
-              onClick={() => deleteRoom(room.id)}
+              onClick={() => deleteRoomHandler(room.id)}
             >
               Delete
             </button>
