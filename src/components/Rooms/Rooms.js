8ae6d5338503100d32197796c54432b5543cb044
Ahmad Zamir Yousufi <ahmadzamiryousofi1397@gmail.com>
2022-10-06 17:08:58 +0430

    add some design to the arrows

--- src/components/Rooms/Rooms.js
+++ src/components/Rooms/Rooms.js
@@ -3,6 +3,8 @@ import { useSelector, useDispatch } from 'react-redux';
 import { GrFacebookOption } from 'react-icons/gr';
 import { FaTwitter } from 'react-icons/fa';
 import { AiFillLinkedin } from 'react-icons/ai';
+import { BiRightArrow } from "react-icons/bi";
+import { BiLeftArrow } from "react-icons/bi";
 import { getRooms } from '../../redux/rooms';
 import Carousel from 'react-multi-carousel';
 import 'react-multi-carousel/lib/styles.css';
@@ -33,27 +35,95 @@ const Rooms = () => {
   useEffect(() => {
     dispatch(getRooms());
   }, [dispatch]);
+  // change the arrow buttons in the carousel to the BiRightArrow and BiLeftArrow icons with the color ##97BF11
+  const CustomRightArrow = ({ onClick, ...rest }) => {
+    const {
+      onMove,
+      carouselState: { currentSlide, deviceType }
+    } = rest;
+    // onMove means if dragging or swiping in progress.
+    return (
+      <button
+        className="btn btn-primary"
+        onClick={() => onClick()}
+        style={{
+          position: "absolute",
+          top: "50%",
+          right: "0",
+          transform: "translateY(-50%)",
+          zIndex: "1",
+          backgroundColor: "#97BF11",
+          border: "none",
+          borderRadius: "50% 0 0 50%",
+          width: "90px",
+          height: "60px",
+          fontSize: "20px",
+          color: "#fff",
+          fontWeight: "bold",
+          boxShadow: "0 0 10px 0 rgba(0, 0, 0, 0.2)"
+        }}
+      >
+        <BiRightArrow />
+      </button>
+    );
+  };
+
+  const CustomLeftArrow = ({ onClick, ...rest }) => {
+    const {
+      onMove,
+      carouselState: { currentSlide, deviceType }
+    } = rest;
+    return (
+      <button
+        className="btn btn-primary"
+        onClick={() => onClick()}
+        style={{
+          position: "absolute",
+          top: "50%",
+          left: "0",
+          transform: "translateY(-50%)",
+          zIndex: "1",
+          backgroundColor: "#97BF11",
+          border: "none",
+          // borderRaduis only from the right side of the button
+          borderRadius: "0 50% 50% 0",
+          width: "90px",
+          height: "60px",
+          fontSize: "20px",
+          color: "#fff",
+          fontWeight: "bold",
+          boxShadow: "0 0 10px 0 rgba(0, 0, 0, 0.2)"
+        }}
+      >
+        <BiLeftArrow />
+      </button>
+    );
+  };
+      
   return (
     <div>
       <h1 className="text-center mt-5 mb-2 pt-5 fw-bold fs-1">Rooms</h1>
       <h2 className="text-center mt-2 mb-5 fw-bold opacity-50 fs-5">Please select a Room</h2>
       <Carousel
         responsive={responsive}
+        infinite={true}
+        autoPlay={true}
+        autoPlaySpeed={3000}
+        keyBoardControl={true}
+        customTransition="all .5"
+        transitionDuration={500}
+        containerClass="carousel-container"
+        deviceType={responsive.deviceType}
         partialVisible={true}
         swipeable={true}
         showDots={true}
-        // ssr={true} // means to render carousel on server-side.
-        // infinite={true}
-        // autoPlay={this.props.deviceType !== "mobile" ? true : false}
-        // autoPlaySpeed={1000}
-        // keyBoardControl={true}
-        // customTransition="all .5"
-        transitionDuration={500}
-        containerClass="carousel-container"
-        // deviceType={this.props.deviceType}
         dotListClass="custom-dot-list-style position-absolute top-50 start-50 translate-middle"
         renderDotsOutside={true}
         itemClass="carousel-item-padding-40-px"
+        slidesToSlide={1}
+        arrows={true}
+        customRightArrow={<CustomRightArrow />}
+        customLeftArrow={<CustomLeftArrow />}
       >
         {rooms.map((room) => (
           <a href={`/room/${room.id}`} key={room.id} className="text-decoration-none text-dark">
