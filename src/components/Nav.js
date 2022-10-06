70db42525bf711c91dcb4664de33028b27e031ae
Precious Akams <preciousakams@yahoo.com>
2022-10-06 17:29:31 +0100

    nav style added

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -9,23 +9,18 @@ const Nav = () => (
         <div className="list-group list-group-flush mx-3 mt-4">
 
           <a href="/" className="list-group-item list-group-item-action py-2 ripple active" aria-current="true">
-            <i className="fas fa-tachometer-alt fa-fw me-3" />
             <span>Home</span>
           </a>
           <a href="/room-new" className="list-group-item list-group-item-action py-2 ripple ">
-            <i className="fas fa-chart-area fa-fw me-3" />
             <span>Add Room</span>
           </a>
           <a href="/room-delete" className="list-group-item list-group-item-action py-2 ripple">
-            <i className="fas fa-lock fa-fw me-3" />
             <span>Delete Room</span>
           </a>
           <a href="/user" className="list-group-item list-group-item-action py-2 ripple">
-            <i className="fas fa-chart-line fa-fw me-3" />
             <span>Add User</span>
           </a>
           <a href="/reservations-new" className="list-group-item list-group-item-action py-2 ripple">
-            <i className="fas fa-chart-pie fa-fw me-3" />
             <span>Make Reservations</span>
           </a>
         </div>
