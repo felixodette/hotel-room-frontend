55eb7c03baad0bde31031064d54c7db8d4b8c1e0
Ahmad Zamir Yousufi <ahmadzamiryousofi1397@gmail.com>
2022-10-07 17:08:48 +0430

    write test for checking the success rendering of elements on the dom

--- src/components/__tests__/Rooms.test.js
+++ src/components/__tests__/Rooms.test.js
@@ -1,1 +1,20 @@
 import React from 'react';
+import '@testing-library/jest-dom';
+import { render, screen } from '@testing-library/react';
+import Rooms from '../Rooms/Rooms';
+// import renderer from 'react-test-renderer';
+import { Provider } from 'react-redux';
+import store from '../../redux/configureStore';
+
+describe('Rooms', () => {
+  render(
+    <Provider store={store}>
+        <Rooms />
+    </Provider>
+    );
+    test('Test the dom for success rendering elements', () => {
+        const title = screen.getByText('ROOMS');
+        expect(title).toBeInTheDocument();
+        }
+    );
+});
