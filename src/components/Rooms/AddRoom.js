46201159422aa1a01a03f7dc41382e0327a5e737
Huseyin Biyik <tahahuseyinb@gmail.com>
2022-10-07 18:47:47 +0300

    Delete unnecessary divs in add-room form

--- src/components/Rooms/AddRoom.js
+++ src/components/Rooms/AddRoom.js
@@ -43,7 +43,7 @@ function AddRoom() {
       <h2 id="add-room-heading" className="text-center  mt-5 fw-bold fs-1 text-white text-uppercase">Add Room</h2>
       <hr id="add-room-hr" />
       <p className="text-center  fs-6 text-white">
-        Would you like to rent your hotel room to our website?
+        Would you like to sell your hotel room to our website?
         {' '}
         <br />
         No need to wait. Fill the forms below your room will be added to our website accordingly!
@@ -53,114 +53,101 @@ function AddRoom() {
       </p>
       <form
         id="add-room-form"
-        className="col-md-6 d-flex align-items-center flex-column justify-content-center"
+        className="col-md-6 col-sm-12 d-flex align-items-center flex-column justify-content-center"
         onSubmit={submitRoom}
       >
-        <div className="col-10 d-flex align-items-center flex-column justify-content-center mb-3">
-          <label htmlFor="name" className="text-white  col-12">
-            Room Name:
-            {' '}
-            <br />
-            <input
-              id="name"
-              className="col-12 bg-transparent-add-room rounded"
-              type="text"
-              placeholder="Deluxe Double Room"
-              value={name}
-              onChange={(e) => setName(e.target.value)}
-              required
-            />
-          </label>
+        <label htmlFor="name" className="text-white  col-md-8 col-sm-12 mb-3">
+          Room Name:
+          {' '}
+          <br />
+          <input
+            id="name"
+            className="col-12 bg-transparent-add-room rounded"
+            type="text"
+            placeholder="Deluxe Double Room"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            required
+          />
+        </label>
 
-        </div>
-        <div className="col-10 d-flex align-items-center flex-column justify-content-center mb-3">
-          <label htmlFor="description" className="text-white col-12">
-            Room Description:
-            {' '}
-            <br />
-            <input
-              id="description"
-              className="col-12 bg-transparent-add-room rounded"
-              type="text"
-              placeholder="Description"
-              value={description}
-              onChange={(e) => setDescription(e.target.value)}
-              required
+        <label htmlFor="description" className="text-white  col-md-8 col-sm-12 mb-3">
+          Room Description:
+          {' '}
+          <br />
+          <input
+            id="description"
+            className="col-12 bg-transparent-add-room rounded"
+            type="text"
+            placeholder="Description"
+            value={description}
+            onChange={(e) => setDescription(e.target.value)}
+            required
+          />
+        </label>
 
-            />
-          </label>
+        <label htmlFor="image" className="text-white  col-md-8 col-sm-12 mb-3">
+          Room Image:
+          {' '}
+          <br />
+          <input
+            id="image"
+            className="col-12 bg-transparent-add-room rounded"
+            type="text"
+            placeholder="Image URL"
+            value={image}
+            onChange={(e) => setImage(e.target.value)}
+            required
+          />
+        </label>
 
-        </div>
-        <div className="col-10 d-flex align-items-center flex-column justify-content-center mb-3">
-          <label htmlFor="image" className="text-white col-12">
-            Room Image:
-            {' '}
-            <br />
-            <input
-              id="image"
-              className="col-12 bg-transparent-add-room rounded"
-              type="text"
-              placeholder="Image URL"
-              value={image}
-              onChange={(e) => setImage(e.target.value)}
-              required
-            />
-          </label>
+        <label htmlFor="size" className="text-white  col-md-8 col-sm-12 mb-3">
+          Room Size:
+          {' '}
+          <br />
+          <input
+            id="size"
+            className="col-12 bg-transparent-add-room rounded"
+            type="text"
+            placeholder="In square meter"
+            value={size}
+            onChange={(e) => setSize(e.target.value)}
+            required
+          />
+        </label>
 
-        </div>
-        <div className="col-10 d-flex align-items-center flex-column justify-content-center mb-3">
-          <label htmlFor="size" className="text-white col-12">
-            Room Size:
-            {' '}
-            <br />
-            <input
-              id="size"
-              className="col-12 bg-transparent-add-room rounded"
-              type="text"
-              placeholder="In square meter"
-              value={size}
-              onChange={(e) => setSize(e.target.value)}
-              required
-            />
-          </label>
-
-        </div>
-        <div className="col-10 d-flex align-items-center flex-column justify-content-center mb-3">
-          <label htmlFor="view" className="text-white col-12">
-            Room View:
-            {' '}
-            <br />
-            <input
-              id="view"
-              className="col-12 bg-transparent-add-room rounded"
-              type="text"
-              placeholder="Sea, ocean.."
-              value={view}
-              onChange={(e) => setView(e.target.value)}
-              required
-            />
-          </label>
-        </div>
-        <div className="col-10 d-flex align-items-center flex-column justify-content-center mb-3">
-          <label htmlFor="bedding" className="text-white col-12">
-            Bedding Option:
-            {' '}
-            <br />
-            <input
-              id="bedding"
-              className="col-12 bg-transparent-add-room rounded"
-              type="text"
-              placeholder="Double, triple, twin..."
-              value={bedding}
-              onChange={(e) => setBedding(e.target.value)}
-              required
-            />
-          </label>
-
-        </div>
+        <label htmlFor="view" className="text-white  col-md-8 col-sm-12 mb-3">
+          Room View:
+          {' '}
+          <br />
+          <input
+            id="view"
+            className="col-12 bg-transparent-add-room rounded"
+            type="text"
+            placeholder="Sea, ocean.."
+            value={view}
+            onChange={(e) => setView(e.target.value)}
+            required
+          />
+        </label>
+        <label htmlFor="bedding" className="text-white  col-md-8 col-sm-12 mb-3">
+          Bedding Option:
+          {' '}
+          <br />
+          <input
+            id="bedding"
+            className="col-12 bg-transparent-add-room rounded"
+            type="text"
+            placeholder="Double, triple, twin..."
+            value={bedding}
+            onChange={(e) => setBedding(e.target.value)}
+            required
+          />
+        </label>
         <button type="submit" id="add-form-btn">
           Submit
         </button>
+
       </form>
       <span>
         {message}
