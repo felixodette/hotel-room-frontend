ce3f5187141953ac6b6fadcaef3e60503b3ed912
Precious Akams <preciousakams@yahoo.com>
2022-10-10 09:27:53 +0100

    linters fixed

--- src/components/Rooms/Room.js
+++ src/components/Rooms/Room.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
-import '../styles/Room.css';
+// import '../styles/Room.css';
 
 const Room = () => {
   const [roomDetails, setRoomDetails] = useState('');
