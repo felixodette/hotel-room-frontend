a29692ce951149ec9ea07c48f8d719be8e684988
Felix Odete <felix.odette@gmail.com>
2022-10-12 16:30:59 +0300

    Update: Store

--- src/components/store.js
+++ src/components/store.js
@@ -9,6 +9,8 @@ const store = createStore(
   initialState,
   compose(
     applyMiddleware(...middleware),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()));
+/* eslint-disable */
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()));
+/* eslint-enable */
 
 export default store;
