326323d69c4f9543bd8d8ba0faac2efad9410abe
Precious Akams <preciousakams@yahoo.com>
2022-10-12 14:06:48 +0100

    es6 syntax implemented

--- src/components/App.js
+++ src/components/App.js
@@ -9,29 +9,27 @@ import Nav from './Nav';
 import MyReservations from './Reservations/MyReservations';
 import ReservationForm from './Reservations/ReservationForm';
 
-function App() {
-  return (
-    // make the design responsive only for desktop
-    <div className="row m-0 p-0">
-      <div className="col-md-2 w-sm-0 p-0">
-        <Nav />
-      </div>
-      <div className="col-md-10 p-0 vh-100">
-        <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<Rooms />} />
-            <Route path="/reservations" element={<MyReservations />} />
-            <Route path="/reservations-new" element={<ReservationForm />} />
-            <Route path="/room-new" element={<AddRoom />} />
-            <Route path="/room/:id" element={<Room />} />
-            {/* <Route path="/room-list" element={<IndexRemoveRoom />} /> */}
-            <Route path="/room-delete" element={<DeleteRoom />} />
-            <Route path="/user" element={<AddUser />} />
-          </Routes>
-        </BrowserRouter>
-      </div>
+const App = () => (
+  // make the design responsive only for desktop
+  <div className="row m-0 p-0">
+    <div className="col-md-2 w-sm-0 p-0">
+      <Nav />
     </div>
-  );
-}
+    <div className="col-md-10 p-0 vh-100">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Rooms />} />
+          <Route path="/reservations" element={<MyReservations />} />
+          <Route path="/reservations-new" element={<ReservationForm />} />
+          <Route path="/room-new" element={<AddRoom />} />
+          <Route path="/room/:id" element={<Room />} />
+          {/* <Route path="/room-list" element={<IndexRemoveRoom />} /> */}
+          <Route path="/room-delete" element={<DeleteRoom />} />
+          <Route path="/user" element={<AddUser />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  </div>
+);
 
 export default App;
