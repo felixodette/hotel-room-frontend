891134006279ab1021035f9b2d167c5dc4e64925
Felix Odete <felix.odette@gmail.com>
2022-10-12 15:11:11 +0300

    Create: My reservations view

--- src/components/Reservations/UserReservations.js
+++ src/components/Reservations/UserReservations.js
@@ -1,14 +1,26 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { fetchReservations } from "../../actions/reservationActions";
 import Card from 'react-bootstrap/Card';
 
-const UserReservations = () => (
-  <>
-    <h1 className="text-center m-4">Reservations</h1>
-    <div className="reservations-container">
-      <div className="reservation">
+class UserReservations extends Component (
+  componentWillMount() {
+    this.props.fetchReservations();
+  };
+
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.newReservation) {
+      this.props.reservations.unshift(nextProps.newReservation);
+    };
+  };
+
+  render() {
+    const postReservations = this.props.map(post => (
+      <div key={reservation.id}>
         <Card>
           <Card.Header as="h5">
-            Reservation
+            {reservation.description}
           </Card.Header>
           <Card.Body>
             <div className="d-flex justify-content-end">
@@ -17,8 +29,29 @@ const UserReservations = () => (
           </Card.Body>
         </Card>
       </div>
-    </div>
-  </>
-);
+    ));
+    return (
+      <div>
+        <div className="reservations-container">
+          <div className="reservation">
+            <h1>My Reservations</h1>
+            {postReservations}
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
 
-export default UserReservations;
+UserReservations.propTypes = {
+  fetchReservations: PropTypes.func.isRequired,
+  posts: PropTypes.array.isRequired,
+  newReservation: PropTypes.object
+}
+
+const mapStateToProps = state => ({
+  posts: state.posts.items,
+  newPost: state.posts.item
+});
+
+export default connect(mapStateToProps, { fetchReservations })(UserReservations);
