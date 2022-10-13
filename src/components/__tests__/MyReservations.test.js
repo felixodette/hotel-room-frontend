4f9012e3ccb905ced0750f3ebb35bdd59b0995e6
Felix Odete <felix.odette@gmail.com>
2022-10-13 12:40:40 +0300

    Test: My reservations

---
+++ src/components/__tests__/MyReservations.test.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import '@testing-library/jest-dom';
+import { render, screen } from '@testing-library/react';
+import renderer from 'react-test-renderer';
+import { Provider } from 'react-redux';
+import { BrowserRouter as Router } from 'react-router-dom';
+import MyReservations from '../Reservations/MyReservations';
+import store from '../../redux/configureStore';
+
+describe('My Reservations', () => {
+  render(
+    <Provider store={store}>
+      <MyReservations />
+    </Provider>,
+  );
+
+  test('Test the dom for success rendering elements', () => {
+    const title = screen.getByText('MY RESERVATIONS');
+    expect(title).toBeInTheDocument();
+  });
+
+  test('Test the dom for non existing element on the page', () => {
+    const title = screen.queryByText('example');
+    expect(title).not.toBeInTheDocument();
+  });
+
+  test('It renders the Reservation component correctly', () => {
+    const { container } = render(
+      <Provider store={store}>
+        <MyReservations />
+      </Provider>,
+    );
+    expect(container).toMatchSnapshot();
+  });
+
+  test('It renders the Reservations Form component correctly', () => {
+    const tree = renderer
+      .create(
+        <Provider store={store}>
+          <MyReservations />
+        </Provider>,
+      )
+      .toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+});
