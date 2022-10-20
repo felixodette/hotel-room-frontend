e78d3d9a2cd64e084fd6a44d28742696e6f0ed78
Ahmad Zamir Yousufi <ahmadzamiryousofi1397@gmail.com>
2022-10-20 20:30:38 +0430

    add the load screen for Rooms page while fetching Rooms from api

--- src/components/Rooms/Rooms.js
+++ src/components/Rooms/Rooms.js
@@ -96,67 +96,84 @@ const Rooms = () => {
     onClick: PropTypes.func.isRequired,
   };
 
+  if (rooms.length === 0) {
+    return (
+      <div className="loading">
+        <div className="spinner-border text-success" role="status">
+          <span className="sr-only">Loading...</span>
+        </div>
+      </div>
+    );
+  } if (rooms.length > 0) {
+    return (
+      <div id="rooms-container" className="w-100">
+        <h1 className="text-center mt-5 mb-2 pt-5 mt-md-1 fw-bold fs-2">ROOMS</h1>
+        <h2 className="text-center mt-2 mb-3 fw-bold opacity-50 fs-5">Please select a Room</h2>
+        <Carousel
+          responsive={responsive}
+          infinite
+          autoPlay
+          autoPlaySpeed={3000}
+          keyBoardControl
+          customTransition="all .5"
+          transitionDuration={500}
+          containerClass="carousel-container ms-2 me-2 pt-2 pb-2"
+          deviceType={responsive.deviceType}
+          partialVisible
+          swipeable
+          showDots
+          dotListClass="custom-dot-list-style carousel-dots"
+          renderDotsOutside
+          itemClass="carousel-item-padding-40-px ps-md-4 pe-md-2 rounded-5 room-card"
+          slidesToSlide={1}
+          arrows
+          customRightArrow={<CustomRightArrow onClick={() => { }} />}
+          customLeftArrow={<CustomLeftArrow onClick={() => { }} />}
+          removeArrowOnDeviceType={['tablet', 'mobile']}
+        >
+          {rooms.map((room) => (
+            <a href={`/room/${room.id}`} key={room.id} className="text-decoration-none text-dark w-100 h-100">
+              <div className="card h-100 border-0 rounded-5 bg-light text-center p-3 p-md-3 transition" key={room.id}>
+                <img src={room.image} alt={room.name} className="card-img-top rounded-4 mb-3 mb-md-4 mx-auto" id="room-image" />
+                <div className="card-body">
+                  <h3 className="card-title fw-bold fs-6 mb-3 mb-md-4">{room.name}</h3>
+                  <p className="card-text fs-7 opacity-50 mb-2 mb-md-2 text-start text-md-center text-lg-start text-xl-center room-description">
+                    {' '}
+                    {room.description}
+                  </p>
+                </div>
+                <div className="card-footer bg-transparent border-0">
+                  <GrFacebookOption
+                    onClick={() => {
+                      window.open(`https://www.facebook.com/sharer/sharer.php?u=${window.location.href}`, '_blank');
+                    }}
+                    className="fs-1 opacity-50 mb-2 mb-md-2 border border-2 border-dark rounded-circle p-2 mx-2 social-icons"
+                  />
+                  <FaTwitter
+                    onClick={() => {
+                      window.open(`https://twitter.com/intent/tweet?url=${window.location.href}`, '_blank');
+                    }}
+                    className="fs-1 opacity-50 mb-2 mb-md-2 border border-2 border-dark rounded-circle p-2 mx-2 social-icons"
+                  />
+                  <AiFillLinkedin
+                    onClick={() => {
+                      window.open(`https://www.linkedin.com/shareArticle?mini=true&url=${window.location.href}`, '_blank');
+                    }}
+                    className="fs-1 opacity-50 mb-2 mb-md-2 border border-2 border-dark rounded-circle mx-2 p-2 social-icons"
+                  />
+                </div>
+              </div>
+            </a>
+          ))}
+        </Carousel>
+      </div>
+    );
+  }
   return (
-    <div id="rooms-container" className="w-100">
+    <div className="text-center">
       <h1 className="text-center mt-5 mb-2 pt-5 mt-md-1 fw-bold fs-2">ROOMS</h1>
       <h2 className="text-center mt-2 mb-3 fw-bold opacity-50 fs-5">Please select a Room</h2>
-      <Carousel
-        responsive={responsive}
-        infinite
-        autoPlay
-        autoPlaySpeed={3000}
-        keyBoardControl
-        customTransition="all .5"
-        transitionDuration={500}
-        containerClass="carousel-container ms-2 me-2 pt-2 pb-2"
-        deviceType={responsive.deviceType}
-        partialVisible
-        swipeable
-        showDots
-        dotListClass="custom-dot-list-style carousel-dots"
-        renderDotsOutside
-        itemClass="carousel-item-padding-40-px ps-md-4 pe-md-2 rounded-5 room-card"
-        slidesToSlide={1}
-        arrows
-        customRightArrow={<CustomRightArrow onClick={() => {}} />}
-        customLeftArrow={<CustomLeftArrow onClick={() => {}} />}
-        removeArrowOnDeviceType={['tablet', 'mobile']}
-      >
-        {rooms.map((room) => (
-          <a href={`/room/${room.id}`} key={room.id} className="text-decoration-none text-dark w-100 h-100">
-            <div className="card h-100 border-0 rounded-5 bg-light text-center p-3 p-md-3 transition" key={room.id}>
-              <img src={room.image} alt={room.name} className="card-img-top rounded-4 mb-3 mb-md-4 mx-auto" id="room-image" />
-              <div className="card-body">
-                <h3 className="card-title fw-bold fs-6 mb-3 mb-md-4">{room.name}</h3>
-                <p className="card-text fs-7 opacity-50 mb-2 mb-md-2 text-start text-md-center text-lg-start text-xl-center room-description">
-                  {' '}
-                  {room.description}
-                </p>
-              </div>
-              <div className="card-footer bg-transparent border-0">
-                <GrFacebookOption
-                  onClick={() => {
-                    window.open(`https://www.facebook.com/sharer/sharer.php?u=${window.location.href}`, '_blank');
-                  }}
-                  className="fs-1 opacity-50 mb-2 mb-md-2 border border-2 border-dark rounded-circle p-2 mx-2 social-icons"
-                />
-                <FaTwitter
-                  onClick={() => {
-                    window.open(`https://twitter.com/intent/tweet?url=${window.location.href}`, '_blank');
-                  }}
-                  className="fs-1 opacity-50 mb-2 mb-md-2 border border-2 border-dark rounded-circle p-2 mx-2 social-icons"
-                />
-                <AiFillLinkedin
-                  onClick={() => {
-                    window.open(`https://www.linkedin.com/shareArticle?mini=true&url=${window.location.href}`, '_blank');
-                  }}
-                  className="fs-1 opacity-50 mb-2 mb-md-2 border border-2 border-dark rounded-circle mx-2 p-2 social-icons"
-                />
-              </div>
-            </div>
-          </a>
-        ))}
-      </Carousel>
+      <h3 className="text-center mt-2 mb-3 fw-bold opacity-50 fs-5">No Rooms Found</h3>
     </div>
   );
 };
